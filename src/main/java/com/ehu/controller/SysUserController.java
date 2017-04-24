5f282950c4da7f626f924439a5e6d773cf59c452
wek <123456>
2017-04-24 15:56:00 +0800

    profile项目配置

--- src/main/java/com/ehu/controller/SysUserController.java
+++ src/main/java/com/ehu/controller/SysUserController.java
@@ -4,14 +4,14 @@ import com.ehu.bean.request.LoginRequest;
 import com.ehu.exceptions.LoginValidationException;
 import com.ehu.service.SysUserService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Date;
 
 /**
  * write something to describe this file.
@@ -30,13 +30,5 @@ public class SysUserController {
     @ApiOperation("登录")
     public Object login(@Valid @RequestBody LoginRequest request) throws LoginValidationException {
         return userService.login(request);
-    }
-
-    @GetMapping("/test")
-    @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "token", value = "token", required = true, dataType = "string", paramType = "header")
-    })
-    public Object test() {
-        return new Date();
     }
 }
